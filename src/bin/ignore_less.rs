ae3451b9a7dffb1a47c9217c7c1385d1515454c7
Dan Munckton <dan@cultivatehq.com>
2017-03-21 13:44:01 +0000

    Add ignore_less.rs

---
+++ src/bin/ignore_less.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+
+use std::{thread, time};
+use rand::{thread_rng, Rng};
+
+fn main() {
+    thread::spawn(|| boring());
+    println!("I'm listening ...");
+    sleep(2000);
+    println!("You're boring; I'm leaving.");
+}
+
+fn boring() {
+    for i in 0.. {
+        println!("boring! {}", i);
+        sleep(thread_rng().gen_range(0, 1000));
+    };
+}
+
+fn sleep(dur_ms: u64) {
+    thread::sleep(time::Duration::from_millis(dur_ms));
+}
