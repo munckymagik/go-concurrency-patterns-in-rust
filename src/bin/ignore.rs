0a40b9ac0e3977d6788c2bc6f104614c520e66b5
Dan Munckton <dan@cultivatehq.com>
2017-03-21 13:39:03 +0000

    Add ignore.rs

---
+++ src/bin/ignore.rs
@@ -0,0 +1,20 @@
+extern crate rand;
+
+use std::{thread, time};
+use rand::{thread_rng, Rng};
+
+fn main() {
+    thread::spawn(|| boring());
+    println!("I got bored");
+}
+
+fn boring() {
+    for i in 0.. {
+        println!("boring! {}", i);
+        sleep(thread_rng().gen_range(0, 1000));
+    };
+}
+
+fn sleep(dur_ms: u64) {
+    thread::sleep(time::Duration::from_millis(dur_ms));
+}
