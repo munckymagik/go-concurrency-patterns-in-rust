17e4e389abd596bb88fa30a64d4420f323f31d50
Dan Munckton <dan@cultivatehq.com>
2017-04-02 12:03:52 +0100

    Add 12_timeout_using_select.rs

---
+++ src/bin/12_timeout_using_select.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate chan;
+extern crate rand;
+
+use std::{thread, time};
+use chan::Receiver;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let c = boring("Joe");
+    let duration = time::Duration::from_millis(500);
+
+    loop {
+        // In each loop, Joe has up to 500 ms to respond or the programme times-out.
+        let timeout = chan::after(duration);
+        chan_select! {
+            c.recv() -> s => println!("{}", s.unwrap()),
+            timeout.recv() => {
+                println!("You're too slow.");
+                return;
+            },
+        }
+    }
+}
+
+fn boring(message: &str) -> Receiver<String> {
+    let message_for_closure = message.to_owned();
+    let (tx, rx) = chan::async();
+
+    thread::spawn(move || {
+        for i in 0.. {
+            tx.send(format!("{} {}", message_for_closure, i));
+            sleep(thread_rng().gen_range(0, 1000));
+        };
+    });
+
+    rx
+}
+
+fn sleep(dur_ms: u64) {
+    thread::sleep(time::Duration::from_millis(dur_ms));
+}
