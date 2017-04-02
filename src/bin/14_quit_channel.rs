aca1e5688991aea7a36ef15d6fb86b18018c813d
Dan Munckton <dan@cultivatehq.com>
2017-04-02 12:42:05 +0100

    Add 14_quit_channel.rs

---
+++ src/bin/14_quit_channel.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate chan;
+extern crate rand;
+
+use std::{thread, time};
+use chan::Receiver;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let (quit_tx, quit_rx) = chan::sync(0);
+    let c = boring("Joe", quit_rx);
+
+    // The loop will iterate printing Joe's messages until the overall timeout occurs.
+    for _ in 0..(thread_rng().gen_range(0, 10)) {
+        println!("{}", c.recv().unwrap());
+    }
+
+    println!("quitting ...");
+    quit_tx.send(());
+}
+
+fn boring(message: &str, quit_rx: Receiver<()>) -> Receiver<String> {
+    let message_for_closure = message.to_owned();
+    let (tx, rx) = chan::async();
+
+    thread::spawn(move || {
+        for i in 0.. {
+            let msg_i = format!("{} {}", message_for_closure, i);
+            chan_select! {
+                tx.send(msg_i) => { /* do nothing */ },
+                quit_rx.recv() => return,
+            }
+            sleep(thread_rng().gen_range(0, 1000));
+        };
+    });
+
+    rx
+}
+
+fn sleep(dur_ms: u64) {
+    thread::sleep(time::Duration::from_millis(dur_ms));
+}
