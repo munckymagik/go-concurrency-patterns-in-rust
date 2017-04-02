646671d40e68f6c825a17c944bd4aa71ba1b9d6e
Dan Munckton <dan@cultivatehq.com>
2017-04-02 10:52:21 +0100

    Add 11_select using chan

---
+++ src/bin/11_select.rs
@@ -0,0 +1,80 @@
+/*
+ * Now using chan_select! from the chan crate in fan_in.
+ */
+
+#[macro_use]
+extern crate chan;
+extern crate rand;
+
+use std::{thread, time};
+use chan::{Sender, Receiver};
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let c = fan_in(boring("Joe"), boring("Ann"));
+
+    for _ in 0..10 {
+        // Retrieve 2 messages at a time from queue
+        let msg1 = c.recv().expect("channel closed getting msg1");
+        let msg2 = c.recv().expect("channel closed getting msg2");
+
+        println!("{}", msg1.message);
+        println!("{}", msg2.message);
+
+        // Send the continuation messages. These block until the receiver reads.
+        msg1.tx_continue.send(true);
+        msg2.tx_continue.send(true);
+    }
+    println!("You're both boring; I'm leaving.");
+}
+
+fn fan_in<T: 'static + Send>(input1: Receiver<T>, input2: Receiver<T>) -> Receiver<T> {
+    let (tx, rx) = chan::async();
+
+    thread::spawn(move || loop {
+        chan_select! {
+            input1.recv() -> msg => tx.send(msg.expect("msg from input1")),
+            input2.recv() -> msg => tx.send(msg.expect("msg from input2")),
+        }
+    });
+
+    rx
+}
+
+struct Message {
+    /// The message text to send
+    message: String,
+    /// The producing thread will block until the receiving thread sends a continuation message
+    /// over this channel
+    tx_continue: Sender<bool>,
+}
+
+fn boring(message: &str) -> Receiver<Message> {
+    let message_for_closure = message.to_owned();
+    let (tx, rx) = chan::async();
+
+    // Sets the buffer size to 0 to create a 'rendezvous' channel.
+    let (tx_continue, rx_continue) = chan::sync(0);
+
+    thread::spawn(move || {
+        for i in 0.. {
+            let msg = Message {
+                message: format!("{} {}", message_for_closure, i),
+                tx_continue: tx_continue.to_owned(),
+            };
+
+            tx.send(msg);
+
+            sleep(thread_rng().gen_range(0, 1000));
+
+            // Block here until the receiver has sent a continuation message
+            rx_continue.recv().expect("boring wait");
+        }
+    });
+
+    rx
+}
+
+fn sleep(dur_ms: u64) {
+    thread::sleep(time::Duration::from_millis(dur_ms));
+}
